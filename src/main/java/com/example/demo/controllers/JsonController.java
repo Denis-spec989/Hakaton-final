c6789a13ac9308c09cde5dfdc2f2a436ade9e515
Alyona <borisovsk_a@mail.ru>
2022-04-23 15:51:35 +0300

    json controller

--- src/main/java/com/example/demo/controllers/JsonController.java
+++ src/main/java/com/example/demo/controllers/JsonController.java
@@ -1,6 +1,12 @@
 package com.example.demo.controllers;
 
+import com.example.demo.dto.PetrolStationDto;
 import com.example.demo.entities.PetrolStationEntity;
+import com.example.demo.services.PetrolStationService;
+import com.example.demo.services.impl.PetrolStationServiceImpl;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,19 +14,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class JsonController {
+    @Autowired
+    private PetrolStationServiceImpl petrolStationService;
+
     @PostMapping(value = "/newstation", consumes = "application/json")
-    public ResponseEntity<Object> createPerson(@RequestBody PetrolStationEntity petrolStationEntity) {
-//        personService.createPerson(newPerson);
-//        String location = ServletUriComponentsBuilder
-//                .fromCurrentRequest()
-//                .path("/{id}")
-//                .buildAndExpand(newPerson.getId())
-//                .toUriString();
-//        HttpHeaders responseHeaders = new HttpHeaders();
-//        responseHeaders.set(HttpHeaders.LOCATION, location);
+    public ResponseEntity<Object> createStation (@RequestBody ArrayList<PetrolStationDto> petrolStationEntity) {
+        petrolStationService.save(petrolStationEntity);
         return ResponseEntity.status(HttpStatus.CREATED).body("created");
     }
 }
