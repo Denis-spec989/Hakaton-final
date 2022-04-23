14d6a9e3ad32778a59921d6f41b372d283223f67
Denis Ignatov <johndoe@example.com>
2022-04-23 19:34:35 +0300

    before checking

--- src/main/java/com/example/demo/services/XmlService.java
+++ src/main/java/com/example/demo/services/XmlService.java
@@ -4,7 +4,6 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+
 public interface XmlService {
 }
