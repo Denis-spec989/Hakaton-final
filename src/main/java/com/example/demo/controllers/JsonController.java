8ed8aa8ab7c542d900613de46413011f5e183431
Alyona <borisovsk_a@mail.ru>
2022-04-24 09:26:27 +0300

    json as multipart init

--- src/main/java/com/example/demo/controllers/JsonController.java
+++ src/main/java/com/example/demo/controllers/JsonController.java
@@ -3,18 +3,19 @@ package com.example.demo.controllers;
 import com.example.demo.dto.PetrolStationDto;
 import com.example.demo.requests.PetrolStationRequest;
 import com.example.demo.services.PetrolStationService;
+import com.example.demo.services.convertors.impl.models.Csv;
 import com.example.demo.services.convertors.impl.models.Json;
+import com.example.demo.services.convertors.impl.models.JsonMultip;
 import com.example.demo.services.impl.PetrolStationServiceImpl;
 import io.swagger.annotations.ApiOperation;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 @RestController
@@ -31,6 +32,17 @@ public class JsonController {
     public ResponseEntity<Object> createStationJson(@RequestBody ArrayList<PetrolStationRequest> petrolStationRequests) {
         petrolStationService.load(
                 new Json(petrolStationRequests)
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body("created");
+    }
+
+    @ApiOperation(
+            value = "upload data via json multipart"
+    )
+    @PostMapping(value = "", consumes = "multipart/form-data")
+    public ResponseEntity<Object> createStationJsonFile(@RequestParam("file") MultipartFile file) throws IOException {
+        petrolStationService.load(
+                new JsonMultip(file)
         );
         return ResponseEntity.status(HttpStatus.CREATED).body("created");
     }
---
+++ src/main/java/com/example/demo/services/convertors/impl/JsonMultipConvertor.java
@@ -0,0 +1,31 @@
+package com.example.demo.services.convertors.impl;
+
+import com.example.demo.dto.PetrolStationDto;
+import com.example.demo.services.convertors.Converter;
+import com.example.demo.services.convertors.impl.models.Csv;
+import com.example.demo.services.convertors.impl.models.JsonMultip;
+
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+public class JsonMultipConvertor implements Converter<JsonMultip, Iterable<PetrolStationDto>> {
+
+    @Override
+    public ArrayList<PetrolStationDto> convert(JsonMultip input) throws IOException {
+        BufferedReader fileReader = new BufferedReader(new InputStreamReader(input.getMultipartFile().getInputStream(), StandardCharsets.UTF_8));
+             ArrayList<PetrolStationDto> stations = new ArrayList<PetrolStationDto>();
+             return stations;
+        }
+
+    @Override
+    public String getInputType() {
+        return Csv.class.getName();
+    }
+
+    @Override
+    public String getOutputType() {
+        return Iterable.class.getName();
+    }
+}
