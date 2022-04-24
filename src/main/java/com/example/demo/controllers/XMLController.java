e0a201700bebf195866da652e01e52ab8976e343
Denis Ignatov <johndoe@example.com>
2022-04-24 10:41:52 +0300

    endReadme

--- src/main/java/com/example/demo/controllers/XMLController.java
+++ src/main/java/com/example/demo/controllers/XMLController.java
@@ -6,14 +6,14 @@ import io.swagger.annotations.ApiOperation;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.transaction.Transaction;
 import java.io.IOException;
+import java.net.http.HttpRequest;
 
 @RestController
 @RequestMapping("/api/v1/xml")
@@ -32,4 +32,7 @@ public class XMLController {
         );
         return ResponseEntity.status(HttpStatus.CREATED).body("created");
     }
+
+
+
 }
